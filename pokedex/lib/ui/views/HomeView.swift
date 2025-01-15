38b46534d41388732de9e0501334d2cdcaea5582
Mario Air <mario.espasa.2003@gmail.com>
2025-01-15 23:34:57 +0100

    first commit

---
+++ pokedex/lib/ui/views/HomeView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  pokedex
+//
+//  Created by Mario Espasa Planells on 15/1/25.
+//
+
+import SwiftUI
+import SwiftData
+
+struct HomeView: View {
+    
+
+    var body: some View {
+        Text("Hello")
+    }
+
+}
+
+#Preview {
+    HomeView()
+}
